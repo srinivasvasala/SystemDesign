8febb0bd87335f02d0d90b8a584bf2e9688981d7
srinivasvasala <vasalasrinivas53@gmail.com>
2025-02-20 21:13:15 +0530

    EnumSingleton

--- src/main/java/Singleton/EnumSingleton.java
+++ src/main/java/Singleton/EnumSingleton.java
@@ -6,6 +6,9 @@ public enum EnumSingleton {
     public void doSomething() {
         System.out.println("EnumSingleton is doing something");
     }
+    protected Object readResolve() {
+        return INSTANCE;
+    }
     public static void main(String[] args) {
         EnumSingleton.INSTANCE.doSomething();
     }
